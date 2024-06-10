dc8f25d5d5a3729ffe4df77bcc45e2025d3b6619
laptop_martin <martin.fq1908@gmail.com>
2024-06-10 12:44:25 -0500

    feat: inicio conexion con neo4j

--- martin-backend-flask/app/models.py
+++ martin-backend-flask/app/models.py
@@ -1,5 +1,5 @@
 import pickle
-
+from ..config import db
 class ModeloML:
     def __init__(self, model_path):
         with open(model_path, 'rb') as file:
@@ -11,3 +11,31 @@ class ModeloML:
             return resultado, None
         except Exception as e:
             return None, str(e)
+
+# models.py
+
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    @staticmethod
+    def create_user(username, email):
+        with db.driver.session() as session:
+            session.run(
+                "CREATE (u:User {username: $username, email: $email})",
+                username=username, email=email
+            )
+
+    @staticmethod
+    def get_user_by_username(username):
+        with db.driver.session() as session:
+            result = session.run(
+                "MATCH (u:User {username: $username}) RETURN u.username AS username, u.email AS email",
+                username=username
+            )
+            record = result.single()
+            if record:
+                return User(record["username"], record["email"])
+            return None
