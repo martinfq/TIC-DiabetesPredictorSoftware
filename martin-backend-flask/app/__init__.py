18a75083936e1a928edba06ffdf43c4d9429fabf
Martin <martin.fq1908@gmail.com>
2024-07-08 21:47:38 -0500

    feat: inicio auth

--- martin-backend-flask/app/__init__.py
+++ martin-backend-flask/app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from .routes import prediction_routes, user_routes, testdb
+from .routes import prediction_routes, user_routes, testdb, auth_routes
 import os
 from flask_login import LoginManager
-
+from flask_jwt_extended import JWTManager
 def create_app():
     app = Flask(__name__)
     #app.config.from_object('config.Config')
     app.register_blueprint(testdb.main_blueprint)
     app.register_blueprint(prediction_routes.data_blueprint)
     app.register_blueprint(user_routes.user_blueprint)
+    app.register_blueprint(auth_routes.auth_bp)
+
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'a_very_secret_key'
     app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    jwt = JWTManager(app)
     return app
--- martin-backend-flask/app/services/user.py
+++ martin-backend-flask/app/services/user.py
@@ -2,8 +2,7 @@ from ..db.neo4j import db
 
 
 class User:
-    def __init__(self, username, email, nombre, apellido, contraseña, fecha_nacimiento=None, genero=None):
-        self.username = username
+    def __init__(self, email, nombre, apellido, contraseña, fecha_nacimiento=None, genero=None):
         self.email = email
         self.nombre = nombre
         self.apellido = apellido
@@ -12,11 +11,10 @@ class User:
         self.genero = genero
 
     @staticmethod
-    def create_user(username, email, nombre, apellido, contraseña, fecha_nacimiento=None, genero=None):
+    def create_user(email, nombre, apellido, contraseña, fecha_nacimiento=None, genero=None):
         db.execute_write(
             """
             CREATE (u:User {
-                username: $username, 
                 email: $email,
                 nombre: $nombre,
                 apellido: $apellido,
@@ -26,7 +24,6 @@ class User:
             })
             """,
             {
-                "username": username,
                 "email": email,
                 "nombre": nombre,
                 "apellido": apellido,
@@ -35,32 +32,74 @@ class User:
                 "genero": genero
             }
         )
-        return User(username, email, nombre, apellido, contraseña, fecha_nacimiento, genero)
+        return User(email, nombre, apellido, contraseña, fecha_nacimiento, genero)
 
     def get_user(query, value):
         try:
             result = db.execute_read(
                 f"""
                        MATCH (u:User {{{query}: $value}}) 
-                       RETURN u.username AS username, u.email AS email, u.nombre AS nombre, u.apellido AS apellido, 
+                       RETURN u.email AS email, u.nombre AS nombre, u.apellido AS apellido, 
                               u.contraseña AS contraseña, u.fecha_nacimiento AS fecha_nacimiento, u.genero AS genero
                        """,
                 {"value": value}
             )
             if result:
                 record = result[0]
-                return User(record["username"], record["email"], record["nombre"], record["apellido"],
+                return User(record["email"], record["nombre"], record["apellido"],
                             record["contraseña"], record["fecha_nacimiento"], record["genero"])
             return None
         except Exception as e:
-            # Manejo de excepciones
             print(f"Error al obtener el usuario: {e}")
             return None
 
     @staticmethod
-    def get_user_by_username(username):
-        return User.get_user("username", username)
-
-    @staticmethod
     def get_user_by_email(email):
         return User.get_user("email", email)
+
+    @staticmethod
+    def get_all_users():
+        try:
+            results, error = db.execute_read(
+                """
+                MATCH (u:User)
+                RETURN u.email AS email, u.nombre AS nombre, u.apellido AS apellido, 
+                       u.contraseña AS contraseña, u.fecha_nacimiento AS fecha_nacimiento, u.genero AS genero
+                """
+            )
+            users = []
+            for record in results:
+                users.append(User(
+                    record["email"], record["nombre"], record["apellido"],
+                    record["contraseña"], record["fecha_nacimiento"], record["genero"]
+                ))
+            return users
+        except Exception as e:
+            print(f"Error al obtener todos los usuarios: {e}")
+            return []
+
+    @staticmethod
+    def update_user(email,new_nombre, new_apellido, new_contraseña, new_fecha_nacimiento=None,
+                    new_genero=None):
+        try:
+            db.execute_write(
+                """
+                MATCH (u:User {email: $email})
+                SET u.nombre = $new_nombre,
+                    u.apellido = $new_apellido,
+                    u.contraseña = $new_contraseña,
+                    u.fecha_nacimiento = $new_fecha_nacimiento,
+                    u.genero = $new_genero
+                """,
+                {
+                    "new_nombre": new_nombre,
+                    "new_apellido": new_apellido,
+                    "new_contraseña": new_contraseña,
+                    "new_fecha_nacimiento": new_fecha_nacimiento,
+                    "new_genero": new_genero
+                }
+            )
+            return User.get_user_by_email(email)
+        except Exception as e:
+            print(f"Error al actualizar el usuario: {e}")
+            return None
