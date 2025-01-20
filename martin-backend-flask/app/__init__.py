f40571566f3d115fcb6e89a00892b703fa830c77
Martin <martin.fq1908@gmail.com>
2025-01-20 00:19:53 -0500

    Inicio de cosas para produccion

--- martin-backend-flask/app/__init__.py
+++ martin-backend-flask/app/__init__.py
@@ -1,21 +1,24 @@
 from flask import Flask
 from .routes import prediction_routes, user_routes, testdb, auth_routes
-import os
 from flask_jwt_extended import JWTManager
 from .services.login_manager import login_manager
 from datetime import timedelta
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_object('config.Config')
+    load_dotenv()
+    # app.config.from_object('config.Config')
     CORS(app)
     app.register_blueprint(testdb.main_blueprint)
     app.register_blueprint(prediction_routes.data_blueprint)
     app.register_blueprint(user_routes.user_blueprint)
     app.register_blueprint(auth_routes.auth_bp)
 
+    app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'a_very_secret_key'
     app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
